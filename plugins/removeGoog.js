a1b9af1fc684465f8a01f5e60be45f1b0e4618f8
AzureAD\AlejandroCorsino <acorsino@lmi3d.com>
2018-03-26 18:09:29 -0700

    exports

--- plugins/removeGoog.js
+++ plugins/removeGoog.js
@@ -3,34 +3,35 @@ module.exports = function (babel) {
     
     const isGoogExpression = (exp,e) => {
             if(t.isCallExpression(e.expression)){
-                if (e.expression.callee.object.name === 'goog'){
+                if (e.expression.callee.object && e.expression.callee.object.name === 'goog'){
                     if (e.expression.callee.property.name === exp) {
-                        // console.log('IS GOOG EXP:', exp,':', e)
+                        console.log('IS GOOG EXP:', exp,':', e)
                         return true;
                       }
                   }
               }
               return false;
     };
- 
   
-    let className = '';
+   
   let imports = [];
-  	let membExp = [];
+  let exports = []
+  let membExp = [];
   let membExpState = 0;
     return {
       name: "ast-transform", // not required
       visitor: {
         Program(path) {
           
-        //   console.log('PROGRAM',path)
+         // console.log('PROGRAM',path)
           
           path.node.body.forEach( function (e, i){
+                  // Search for Import refs and save it
                 if (isGoogExpression('require', e)){
                     let gPath = path.get('body')[i]
                     let nameSpace = e.expression.arguments[0].value.split('.');
                   	imports.push(nameSpace.join('/'));
-                  	 className = nameSpace[nameSpace.length - 1]
+                  	let className = nameSpace[nameSpace.length - 1]
                   	console.log('E:',nameSpace);
                  
                    	//gPath.traverse(updateGoogRequire,gPath.node)
@@ -40,36 +41,54 @@ module.exports = function (babel) {
                           , t.stringLiteral(nameSpace.join('/')))
                      );
                 }
+            
+               //Search for Exports and save it
+                if (isGoogExpression('provide', e)){
+                    let gPath = path.get('body')[i]
+                    let nameSpace = e.expression.arguments[0].value.split('.');
+                    exports.push(nameSpace.join('/'));
+                  	let className = nameSpace[nameSpace.length - 1]
+                      console.log('EXPORTS:',exports);
+                }
          
           })//forEach
                  
         },
         MemberExpression:{
-        	
-             
            enter(path) {
              membExpState++;
-            	//  console.log("Entered!",membExpState);
+            	 //console.log("Entered!",membExpState);
              	membExp.push(path.node.property.name)
-                // console.log('MemeberExpresson:',path.node.property.name)// === className
+               // console.log('MemeberExpresson:',path.node.property.name)// === className
 
                 if(t.isIdentifier(path.node.object)){
                   membExp.push(path.node.object.name)
-                    // console.log('MemeberExpresson:OBJECT:', path.node.object.name)
+                    //console.log('MemeberExpresson:OBJECT:', path.node.object.name)
                 }
 
             },
-            exit() {
+            exit(path) {
               membExpState--;
               if (membExpState == 0){
-              	// console.log("Exited!",membExp);
+              	//console.log("Exited!",membExp);
                 let m = membExp.reverse().join('/');
-                // console.log('NAME:',m)
-                let found = imports.find(function(element) {
+                console.log('NAME:',m);
+                
+                function findE(element) {
                   return element === m;
-                });
-                // console.log('IMPORTs',imports)
-                found && console.log('FOUND', found);
+                }
+                
+                let foundImport = imports.find(findE);
+                let foundExport = exports.find(findE);
+                //console.log('IMPORTs',imports)
+                if (foundImport){
+                  console.log('FOUND IMPORT',foundImport,':',path,':',membExp[membExp.length -1]);
+                  path.replaceWith(t.identifier(membExp[membExp.length -1]))
+                }
+                 if (foundExport){
+                  console.log('FOUND EXPORT',foundExport,':',path,':',membExp[membExp.length -1]);
+                  path.replaceWith(t.identifier(membExp[membExp.length -1]))
+                }
                 membExp =[];
               }
             }
